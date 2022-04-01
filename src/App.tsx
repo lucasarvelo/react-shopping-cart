23cf28f4a047ff1da4ce1a8e442edd4329ef2e8f
Lucas Arvelo <contact@lucasarvelo.com>
2022-04-01 13:21:41 +0100

    add dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,20 @@ import Badge from "@mui/icons-material/Badge";
 
 import { Wrapper } from "./App.styles";
 
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType> => {
+  return await (await fetch("https://fakestoreapi.com/products")).json();
+};
+
 const App = () => {
   return <div className="App"></div>;
 };
